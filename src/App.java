64ff74f929b40c1e8fb7d0497aa24ec44e05747d
deybivicioso <darlexstv@gmail.com>
2022-11-02 12:15:16 -0400

    completo50Xporciento

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         //ventana1 resultado=new ventana1();
        // ventana2 resultado1=new ventana2();
-        ventana3 resultado2=new ventana3();
+        //ventana3 resultado2=new ventana3();
     }
 }
